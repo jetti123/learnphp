de7a7c2e58657ea4b669d8f45cbc88d62cacc0fc
Jane Etti <etti.jane@gmail.com>
2024-03-08 09:11:28 +0200

    add box

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
-echo 'Hello World!' . time();
-?>
+
+class Box {
+    public $width;
+    public $height;
+    public $length;
+    public $material;
+
+    public function describe(){
+        echo 'width:' . $this->width . 'height:' . $this->height . 'length:' . $this->length;
+    }
+
+}
+
+$box1 = new Box();
+$box1->width = 1;
+$box1->height = 2;
+$box1->length = 3;
+$box1->material = 'wood';
+var_dump($box1);
+$box1 = new Box();
+$box1->width = 5;
+$box1->height = 6;
+$box1->length = 7;
+$box1->material = 'metal';
+var_dump($box2);
+var_dump($box1);
