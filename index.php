f9a70df8eaec911e6dea926434ea9c1120a77b56
Jane Etti <etti.jane@gmail.com>
2024-03-08 11:01:04 +0200

    static

--- index.php
+++ index.php
@@ -1,38 +1,38 @@
 <?php
 
-//library
+class Box {
+    public $size;
+    public static $number;
 
-class Job {
-    public function task(Logger $logger){
-        for($i = 0; $i<10; $i++){                       
-            $logger->log("task done! #  $i");
-        }
+    public static function getNumber(){
+        echo self::$number;
     }
-}
-
-interface Logger {
-    public function log ($message);
-}
-
-class ConsoleLogger implements Logger {
-    public function log($message){
-        echo $message . "\n";
+    public function getSize(){
+        echo $this->size . self::$number;
     }
-}
-//usage code
-class NothingLogger {
-    public function log($message){
+    public static function me1(){
+        var_dump(self::class);
     }
+    public static function me2(){
+        var_dump(static::class);
+    } 
 }
 
-class FileLogger implements Logger {
-    public function log($message){
-        $file = fopen("log.txt", "a");
-        fwrite($file, $message . "\n");
-        fclose($file);
-    }
+class MetalBox extends Box {
+
 }
 
-$job = new Job;
-$logger = new ConsoleLogger();
-$job->task($logger);
+$box1 = new Box();
+$box1->size = 10;
+Box::$number = 10;
+$box1->getSize();
+Box::getNumber();
+//$box2 = new Box();
+//$box2->size = 20;
+Box::$number = 20;
+//var_dump($box1, $box2);
+var_dump(Box::$number, Box::$number);
+Box::me1();
+Box::me2();
+MetalBox::me1();
+MetalBox::me2();
