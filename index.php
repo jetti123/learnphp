722a8150c117c51d4b5e8d5601174693ee2f2f73
Jane Etti <etti.jane@gmail.com>
2024-03-08 11:26:18 +0200

    private function example

--- index.php
+++ index.php
@@ -1,46 +1,22 @@
 <?php
 
 class Box {
-    public $size;
-    protected $width;
-    public static $number;
-
-    public static function getNumber(){
-        echo self::$number;
+    private $weight;
+    public function getWeight(){
+        return $this->weight . 'kg';
     }
-    public function getSize(){
-        echo $this->size . self::$number;
+    public function setWeight($weight){
+        if($weight > 0){
+            $this->weight = $weight;
+        } else {
+            throw new Exception('Negative Mass error');
+        }
     }
-    public static function me1(){
-        var_dump(self::class);
-    }
-    public static function me2(){
-        var_dump(static::class);
-    } 
-    public function getWidth(){
-        var_dump($this->width);
-
-    }
+    
 }
 
-class MetalBox extends Box {
+//sb else code
 
-}
-
-$box1 = new Box();
-$box1->size = 10;
-$box1->getWidth();
-$metal1 = new MetalBox();
-$metal1->getWidth();
-Box::$number = 10;
-$box1->getSize();
-Box::getNumber();
-//$box2 = new Box();
-//$box2->size = 20;
-Box::$number = 20;
-//var_dump($box1, $box2);
-var_dump(Box::$number, Box::$number);
-Box::me1();
-Box::me2();
-MetalBox::me1();
-MetalBox::me2();
+$box = new Box();
+$box->setWeight (-10);
+var_dump($box);
